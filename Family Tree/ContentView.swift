edac735c37506a41d30f8e505539dadd6ae48383
YuretsKravets <y.d.kravets@gmail.com>
2020-04-01 01:13:50 +0300

    Initial Commit

---
+++ Family Tree/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Family Tree
+//
+//  Created by Yurii Kravets on 01/04/2020.
+//  Copyright © 2020 Yurii Kravets. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
